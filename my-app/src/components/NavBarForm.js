74c856ce3fc434b1a3d1d68fea1bee23fa87f7fb
Kayleigh Taylor <ktay0557@gmail.com>
2024-03-05 10:06:55 +0000

    Refactor code so rendered form is created using nested components

--- my-app/src/components/NavBarForm.js
+++ my-app/src/components/NavBarForm.js
@@ -1,42 +1,34 @@
 import React, { Component } from "react";
 import css from "./css/NavBarForm.module.css";
+import NavBarChild from "./NavBarChild";
+
 
 class NavBarForm extends Component {
-
     constructor(props) {
         super(props)
-
+        
         this.state = {
             isLoggedIn: true,
         }
     }
-
     handleButtonClick = () => {
         this.setState((prevState) => ({
             isLoggedIn: prevState.isLoggedIn ? false: true
-        }), () => console.log(this.state.isLoggedIn));
+        }),  () => console.log(this.state.isLoggedIn))
     }
 
     render() {
         return (
-            <div className = {css.NavBar}>
+            <div className= {css.NavBar}>
                 <h1>My Gallery</h1>
 
-                {
-                    this.state.isLoggedIn ?
-                        <button onClick={() => this.handleButtonClick()}>Login</button>
-                        :
-                        <form>
-                           <label htmlFor = "username">Username:</label>
-                           <input placeholder="username" id="username"/>
+                <NavBarChild
+                isLoggedIn={this.state.isLoggedIn}
+                handleClick={this.handleButtonClick}
+                />
 
-                           <label htmlFor = "password">Password:</label>
-                           <input placeholder="password" id="password"/>
-                           <button onClick={() => this.handleButtonClick()}>Submit</button>
-                        </form>
-                }        
             </div>
-        );
+        )
     }
 }
 
